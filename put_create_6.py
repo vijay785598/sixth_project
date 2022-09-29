7fdef9b98f851a660272de8f3c8ddd78810d5fb7
vijay785598 <114460090+vijay785598@users.noreply.github.com>
2022-09-29 17:36:14 +0530

    practice module added
    commit chenges

---
+++ put_create_6.py
@@ -0,0 +1,10 @@
+PutCreate={"CallerId": "Vijay12",
+    "description": "money dedected",
+    "short_description": "ticket creation testing from service now api",
+    "updatedAt": "2022-09-27T03:00:15.504Z"
+}
+
+print(PutCreate['updatedAt'])  #out put= 2022-09-27T03:00:15.504Z
+
+print(PutCreate) #out put= {'CallerId': 'Vijay12', 'description': 'money dedected', 
+#'short_description': 'ticket creation testing from service now api', 'updatedAt': '2022-09-27T03:00:15.504Z'}
